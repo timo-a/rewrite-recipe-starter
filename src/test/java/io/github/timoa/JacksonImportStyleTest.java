92841180c21ec9b1ba889707cfb0ccceea1295d1
timo <1398557+timo-a@users.noreply.github.com>
2024-09-13 01:00:33 +0200

    write tests

---
+++ src/test/java/io/github/timoa/JacksonImportStyleTest.java
@@ -0,0 +1,125 @@
+package io.github.timoa;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.java.OrderImports;
+import org.openrewrite.java.style.ImportLayoutStyle;
+import org.openrewrite.style.NamedStyles;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static java.util.Collections.*;
+import static org.openrewrite.Tree.randomId;
+import static org.openrewrite.java.Assertions.java;
+
+class JacksonImportStyleTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new OrderImports(false));
+    }
+
+    @DocumentExample
+    @Test
+    //this test does not use my defined style but creates one on the fly
+    void inlineStyle() {
+        rewriteRun(
+          spec -> spec.parser(JavaParser.fromJavaVersion().styles(
+            singletonList(
+              new NamedStyles(
+                randomId(),
+                "custom",
+                "custom style",
+                null,
+                emptySet(),
+                singletonList(
+                  ImportLayoutStyle.builder()
+                    .importAllOthers()
+                    .blankLine()
+                    .importPackage("com.fasterxml.jackson.core.*")
+                    .importPackage("tools.jackson.*")
+                    .blankLine()
+                    .importStaticAllOthers()
+                    .build()
+                )
+              )
+            ))),
+          java(
+            """
+              package com.fasterxml.jackson.core.write;
+              
+              import com.fasterxml.jackson.core.*;
+              
+              import java.io.*;
+              
+              import static org.junit.jupiter.api.Assertions.*;
+              
+              import org.junit.jupiter.api.Test;
+              
+              class Test {
+              }
+              """,
+            """
+              package com.fasterxml.jackson.core.write;
+              
+              import java.io.*;
+              import org.junit.jupiter.api.Test;
+              
+              import com.fasterxml.jackson.core.*;
+              
+              import static org.junit.jupiter.api.Assertions.*;
+              
+              class Test {
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    //This tests seeks to use the style I defined. not sure if it is done correctly
+    void myStyle() {
+        rewriteRun(
+          spec -> spec.parser(JavaParser.fromJavaVersion().styles(
+            singletonList(
+              new JacksonImportStyle(
+                randomId(),
+                "custom",
+                "custom style",
+                null,
+                emptySet(),
+                emptyList()))
+            )),
+          java(
+            """
+              package com.fasterxml.jackson.core.write;
+              
+              import com.fasterxml.jackson.core.*;
+              
+              import java.io.*;
+              
+              import static org.junit.jupiter.api.Assertions.*;
+              
+              import org.junit.jupiter.api.Test;
+              
+              class Test {
+              }
+              """,
+            """
+              package com.fasterxml.jackson.core.write;
+              
+              import java.io.*;
+              import org.junit.jupiter.api.Test;
+              
+              import com.fasterxml.jackson.core.*;
+              
+              import static org.junit.jupiter.api.Assertions.*;
+              
+              class Test {
+              }
+              """
+          )
+        );
+    }
+}
