873c02c36ad8b78e9a6026b61cd5c79f9dae3e1f
timo <1398557+timo-a@users.noreply.github.com>
2024-09-15 01:57:51 +0200

    feat(misc): BigDecimal normalizer

---
+++ src/main/java/io/github/timoa/misc/NormalizeBigDecimalCreation.java
@@ -0,0 +1,83 @@
+package io.github.timoa.misc;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.openrewrite.*;
+import org.openrewrite.NlsRewrite.Description;
+import org.openrewrite.NlsRewrite.DisplayName;
+import org.openrewrite.java.JavaTemplate;
+import org.openrewrite.java.JavaVisitor;
+import org.openrewrite.java.MethodMatcher;
+import org.openrewrite.java.tree.Expression;
+import org.openrewrite.java.tree.J;
+
+import java.util.StringJoiner;
+
+@Value
+@EqualsAndHashCode(callSuper = true)
+public class NormalizeBigDecimalCreation extends Recipe {
+
+    @Override
+    public @DisplayName String getDisplayName() {
+        return "Use Strings for BigDecimal instantiation";
+    }
+
+    @Override
+    public @Description String getDescription() {
+        return new StringJoiner("\n")
+                .add("Converts BigDecimal instantiation with a double literal to instantiation with a String. ")
+                .add("CAUTION: This Recipe can change semantics. ")
+                .add("When using doubles to instantiate BigDecimal objects it is not obvious what the result will be. " +
+                        "`new BigDecimal(0.1)` for instance results in a value of " +
+                        "`0.1000000000000000055511151231257827021181583404541015625` and a scale of `55`. " +
+                        "Other than rounding, the scale is also not transparent. While `new BigDecimal(\"1.00\")` will " +
+                        "have a value of `1.00` and a scale 2, neither `new BigDecimal(1.00)` nor `BigDecimal.valueOf(1.00)` will.")
+                .add("This recipe makes the assumption that a developer initializing a BigDecimal with the double " +
+                        "`some.digits` really wants the object resulting from `new BigDecimal(\"some.digit\")` but is not aware of the difference.")
+                .toString();
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return new JavaVisitor<ExecutionContext>() {
+
+            final MethodMatcher VALUE_OF = new MethodMatcher("java.math.BigDecimal valueOf(double)");
+
+            final MethodMatcher CONSTRUCTOR = new MethodMatcher("java.math.BigDecimal <constructor>(double)");
+
+            @Override
+            public J visitMethodInvocation(J.MethodInvocation method, ExecutionContext ctx) {
+                if (VALUE_OF.matches(method)) {
+                    Expression doubleExpression = method.getArguments().get(0);
+                    if (doubleExpression instanceof J.Literal) {
+                        J.Literal doubleLiteral = (J.Literal) method.getArguments().get(0);
+                        return makeBigDecimal(doubleLiteral, getCursor(), method.getCoordinates().replace());
+                    }
+                }
+                return method;
+            }
+
+            @Override
+            public J.NewClass visitNewClass(J.NewClass newClass, ExecutionContext ctx) {
+                if (CONSTRUCTOR.matches(newClass)) {
+                    Expression doubleExpression = newClass.getArguments().get(0);
+                    if (doubleExpression instanceof J.Literal) {
+                        J.Literal doubleLiteral = (J.Literal) newClass.getArguments().get(0);
+                        return makeBigDecimal(doubleLiteral, getCursor(), newClass.getCoordinates().replace());
+                    }
+                }
+                return newClass;
+            }
+
+            private J.NewClass makeBigDecimal(J.Literal doubleLiteral,  org.openrewrite.Cursor scope, org.openrewrite.java.tree.JavaCoordinates coordinates) {
+                String converted = doubleLiteral.getValueSource().replace("d", "");//remove trailing 'd' if present
+                return JavaTemplate
+                        .builder("new BigDecimal(\"" + converted + "\")")
+                        .imports("java.math.BigDecimal")
+                        .build()
+                        .apply(scope, coordinates);
+            }
+        };
+    }
+
+}
---
+++ src/test/java/io/github/timoa/misc/NormalizeBigDecimalCreationTest.java
@@ -0,0 +1,136 @@
+package io.github.timoa.misc;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+public class NormalizeBigDecimalCreationTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new NormalizeBigDecimalCreation())
+          .parser(JavaParser.fromJavaVersion()
+            .logCompilationWarningsAndErrors(true)
+            .classpath("guava"));
+    }
+
+    @DocumentExample
+    @Test
+    void exampleValues() {
+        rewriteRun(
+          spec -> spec
+            .parser(JavaParser.fromJavaVersion()
+              .logCompilationWarningsAndErrors(false)),
+          // language=java
+          java(
+            """
+              import java.math.BigDecimal;
+              
+              class Test {
+                  var constructor_1_00 = new BigDecimal(1.00);
+                  var valueOf_1_00     = BigDecimal.valueOf(1.00);
+                  var constructor_1d   = new BigDecimal(1d);
+              
+              }
+              """,
+            """
+              import java.math.BigDecimal;
+              
+              class Test {
+                  var constructor_1_00 = new BigDecimal("1.00");
+                  var valueOf_1_00     = new BigDecimal("1.00");
+                  var constructor_1d   = new BigDecimal("1");
+              
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void replaceConstructor() {
+        rewriteRun(
+          spec -> spec
+            .parser(JavaParser.fromJavaVersion()
+              .logCompilationWarningsAndErrors(false)),
+          // language=java
+          java(
+            """
+              import java.math.BigDecimal;
+              
+              class Test {
+                  BigDecimal bd1 = new BigDecimal(1.00);
+                  BigDecimal bd2 = new BigDecimal(1d);
+              
+                  void a(){
+                      BigDecimal bd3 = new BigDecimal(1.00);
+                      BigDecimal bd4 = new BigDecimal(1d);
+                  }
+              }
+              """,
+            """
+              import java.math.BigDecimal;
+              
+              class Test {
+                  BigDecimal bd1 = new BigDecimal("1.00");
+                  BigDecimal bd2 = new BigDecimal("1");
+              
+                  void a(){
+                      BigDecimal bd3 = new BigDecimal("1.00");
+                      BigDecimal bd4 = new BigDecimal("1");
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void replaceValueOf() {
+        rewriteRun(
+          spec -> spec
+            .parser(JavaParser.fromJavaVersion()
+              .logCompilationWarningsAndErrors(false)),
+          // language=java
+          java(
+            """
+              import java.math.BigDecimal;
+              
+              class Test {
+                  BigDecimal bd1 = BigDecimal.valueOf(1.00);
+                  BigDecimal bd2 = BigDecimal.valueOf(1d);
+              
+                  void a(){
+                      BigDecimal bd3 = BigDecimal.valueOf(1.00);
+                      BigDecimal bd4 = BigDecimal.valueOf(1d);
+                  }
+              }
+              """,
+            """
+              import java.math.BigDecimal;
+              
+              class Test {
+                  BigDecimal bd1 = new BigDecimal("1.00");
+                  BigDecimal bd2 = new BigDecimal("1");
+              
+                  void a(){
+                      BigDecimal bd3 = new BigDecimal("1.00");
+                      BigDecimal bd4 = new BigDecimal("1");
+                  }
+              }
+              """
+          )
+        );
+    }
+
+
+
+
+
+
+
+}
